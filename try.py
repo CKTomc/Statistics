f716357232d2b375f4ef84b279d43bf17faebe5a
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-01-02 14:23:14 +0100

    Update try.py

--- try.py
+++ try.py
@@ -2,13 +2,17 @@ import numpy as np
 import pandas as pd
 import seaborn as ss
 
-min=<thesmallest>
-max=<thebiggest>
-step=<stepsize>
+min=1
+max=77
+step=3
 arr=np.array(min,max,step)
 
-linspsize=<size>
+linspsize=10
 arr2=np.linspace(min,max,linspsize)
 
 arr3=np.random.rand(5,5) #shape -> (5,5)
+arr4=np.random.randn(5,5) #normal
+arr5=np.diag(arr4) # main diag
+arr6=np.diag(arr4,1) # 1st superior diag
+arr7=np.diag(arr4,-1) # 1st inferior diag
 
