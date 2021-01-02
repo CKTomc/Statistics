f7b32150ea2ecac317c44cd9afdda81e3d35ec75
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-01-02 14:56:00 +0100

    Update try.py

--- try.py
+++ try.py
@@ -16,3 +16,8 @@ arr5=np.diag(arr4) # main diag
 arr6=np.diag(arr4,1) # 1st superior diag
 arr7=np.diag(arr4,-1) # 1st inferior diag
 
+data=pd.read_csv(<location>,head=0)
+data.describe() # picks numerical columns
+data.iloc[2] #3d line using index
+data.loc[<anid>] #line infos using unique info
+data.iloc[0:4] 
