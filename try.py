96444b19b4426d257c264dc6b88444515d832a36
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-01-02 13:47:09 +0100

    Create try.py

---
+++ try.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import seaborn as ss
+
+min=<thesmallest>
+max=<thebiggest>
+step=<stepsize>
+arr=np.array(min,max,step)
+
+linspsize=<size>
+arr2=np.linspace(min,max,linspsize)
+
+arr3=np.random.rand(5,5) #shape -> (5,5)
+
