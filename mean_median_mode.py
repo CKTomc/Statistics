6199808849e2af601e49c9760cd42ea268d27534
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-03-11 15:54:00 +0100

    Update mean_median_mode.py

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -6,23 +6,30 @@ from scipy import stats
 n=randrange(10,2501)
 array=np.random.randint(10001, size=n)
 array=np.sort(array)
+
 mean=round(np.mean(array),1)
 median=round(np.median(array),1)
 mode=stats.mode(array)[0][0]
+
 print(mean)
 print(median)
 print(mode)
 
-"""using test cases"""
+"""using explicit test cases"""
+import sys
 from random import randrange
 import numpy as np
 
-n=randrange(10,2501)
-array=np.random.randint(10001, size=n)
+n=int(input())
+while n<10 or n>2500:
+    n=int(input())
+array=[int(x) for x in input().split()[:n]]
+array=np.array(array)
 array=np.sort(array)
-lg=len(array)
-mean=round(sum(array)/lg,1)
 
+lg=len(array)
+
+mean=round(sum(array)/lg,1)
 
 if lg%2==0:
   median=round((array[int(lg/2)]+array[int(lg/2)-1])/2,1)
