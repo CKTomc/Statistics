263925f9919864b390c195e658ae00aeb77b51af
Khadidja CH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-03-11 14:18:58 +0100

    Update mean_median_mode.py

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -1,3 +1,4 @@
+"""using predefined functions"""
 from random import randrange
 import numpy as np
 from scipy import stats
@@ -11,3 +12,21 @@ mode=stats.mode(array)[0][0]
 print(mean)
 print(median)
 print(mode)
+
+"""using test cases"""
+from random import randrange
+import numpy as np
+from collections import defaultdict
+n=randrange(10,2501)
+array=np.random.randint(10001, size=n)
+array=np.sort(array)
+lg=len(array)
+mean=round(sum(array)/lg,1)
+
+
+if lg%2==0:
+  median=round((array[lg/2]+array[lg/2-1])/2,1)
+else:
+  median=array[round(lg/2)]
+  
+freq=defaultdict(int)
